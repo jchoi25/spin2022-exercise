7ee74a0969b0e7534416e9f424559e8acc6db716
Roland Haas <rhaas@ncsa.illinois.edu>
2022-04-14 12:54:19 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+/usr/bin/env python3
+
+print("""<html>
+<body>
+<p>Hello, Roland!<p/>""")
+
+print("""
+<p>Git log is:
+<pre>
+TODO: add git log here
+</pre>
+</p>
+""")
+
+print("""
+</body>
+</html>""")
