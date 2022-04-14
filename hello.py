0a211fc6c89f5a758d7d5f12a692ea90258c6166
Roland Haas <rhaas@ncsa.illinois.edu>
2022-04-14 13:02:44 -0500

    output log

--- hello.py
+++ hello.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
+from pygit2 import Repository
+repo = Repository('.git')
+
 print("""<html>
 <body>
 <p>Hello, Roland!<p/>""")
 
 print("""
 <p>Git log is:
-<pre>
-TODO: add git log here
+<pre>"""
+
+last = repo[repo.head.target]
+for commit in repo.walk(last.id, pygit2.GIT_SORT_TIME):
+  print(commit.message)
+      
+print("""
 </pre>
 </p>
 """)
