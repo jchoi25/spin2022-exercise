f5f6f53d318851324f88dc48d2d427b40afd6a9f
jchoi25 <juyoung7@illinois.edu>
2022-04-20 11:30:27 -0500

    edited hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from pygit2 import Repository, GIT_SORT_TIME
+repo = Repository('.git')
 
 print("""<html>
 <body>
@@ -6,8 +8,13 @@ print("""<html>
 
 print("""
 <p>Git log is:
-<pre>
-TODO: add git log here
+<pre>""")
+
+last = repo[repo.head.target]
+for commit in repo.walk(last.id, GIT_SORT_TIME):
+    print(commit.message)
+
+print("""
 </pre>
 </p>
 """)
